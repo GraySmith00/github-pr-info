e11be8a2112f6c1d659c5683923344e64b837a1d
Gray Smith <gray.smith00@gmail.com>
2018-12-11 14:49:25 -0700

    Add host to start

--- start.js
+++ start.js
@@ -4,6 +4,7 @@ require('dotenv').config({ path: '.env' });
 // Start the Server
 const app = require('./app');
 const PORT = process.env.port || 3000;
-app.listen(PORT, () => {
+const HOST = '0.0.0.0';
+app.listen(PORT, HOST, () => {
   console.log(`App listening on port: ${PORT}`);
 });
