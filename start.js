f301ee41b10b44e40d02ce6172cf65e6a0d56a67
Gray Smith <gray.smith00@gmail.com>
2018-12-11 14:59:40 -0700

    Fix port naming bug

--- start.js
+++ start.js
@@ -3,8 +3,7 @@ require('dotenv').config({ path: '.env' });
 
 // Start the Server
 const app = require('./app');
-const PORT = process.env.port || 3000;
-const HOST = '0.0.0.0';
-app.listen(PORT, HOST, () => {
-  console.log(`App listening on port: ${PORT}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App listening on port: ${port}`);
 });
